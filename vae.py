b543b7c458a642cfb31c0008bc439fec88d725a8
rifo <horbatenko.vladislav@gmail.com>
2026-02-26 15:21:18 +0100

    VAE with MoG

---
+++ vae.py
@@ -0,0 +1,331 @@
+# Code for DTU course 02460 (Advanced Machine Learning Spring) by Jes Frellsen, 2024
+# Version 1.2 (2024-02-06)
+# Inspiration is taken from:
+# - https://github.com/jmtomczak/intro_dgm/blob/main/vaes/vae_example.ipynb
+# - https://github.com/kampta/pytorch-distributions/blob/master/gaussian_vae.py
+
+import torch
+import torch.nn as nn
+import torch.distributions as td
+import torch.utils.data
+from torch.distributions.mixture_same_family import MixtureSameFamily
+from torch.nn import functional as F
+from tqdm import tqdm
+
+
+class GaussianPrior(nn.Module):
+    def __init__(self, M):
+        """
+        Define a Gaussian prior distribution with zero mean and unit variance.
+
+                Parameters:
+        M: [int] 
+           Dimension of the latent space.
+        """
+        super(GaussianPrior, self).__init__()
+        self.M = M
+        self.mean = nn.Parameter(torch.zeros(self.M), requires_grad=False)
+        self.std = nn.Parameter(torch.ones(self.M), requires_grad=False)
+
+    def forward(self):
+        """
+        Return the prior distribution.
+
+        Returns:
+        prior: [torch.distributions.Distribution]
+        """
+        return td.Independent(td.Normal(loc=self.mean, scale=self.std), 1)
+
+
+class MoGPrior(nn.Module):
+    """
+    Mixture of Gaussians prior for VAE.
+    """
+    def __init__(self, M, K=10, component_std=0.1):
+        super(MoGPrior, self).__init__()
+        self.M = M
+        self.K = K
+        self.component_std = component_std
+        self.logits = nn.Parameter(torch.zeros(K))
+        self.means = nn.Parameter(torch.randn(K, M) * 0.1)
+
+    def forward(self):
+        weights = torch.softmax(self.logits, dim=0)
+        mixture = td.Categorical(probs=weights)
+        components = td.Independent(
+            td.Normal(loc=self.means, scale=torch.full_like(self.means, self.component_std)),
+            1,
+        )
+        return MixtureSameFamily(mixture, components)
+
+
+class GaussianEncoder(nn.Module):
+    def __init__(self, encoder_net):
+        """
+        Define a Gaussian encoder distribution based on a given encoder network.
+
+        Parameters:
+        encoder_net: [torch.nn.Module]             
+           The encoder network that takes as a tensor of dim `(batch_size,
+           feature_dim1, feature_dim2)` and output a tensor of dimension
+           `(batch_size, 2M)`, where M is the dimension of the latent space.
+        """
+        super(GaussianEncoder, self).__init__()
+        self.encoder_net = encoder_net
+
+    def forward(self, x):
+        """
+        Given a batch of data, return a Gaussian distribution over the latent space.
+
+        Parameters:
+        x: [torch.Tensor] 
+           A tensor of dimension `(batch_size, feature_dim1, feature_dim2)`
+        """
+        mean, std = torch.chunk(self.encoder_net(x), 2, dim=-1)
+        return td.Independent(td.Normal(loc=mean, scale=torch.exp(std)), 1)
+
+
+class BernoulliDecoder(nn.Module):
+    def __init__(self, decoder_net):
+        """
+        Define a Bernoulli decoder distribution based on a given decoder network.
+
+        Parameters: 
+        encoder_net: [torch.nn.Module]             
+           The decoder network that takes as a tensor of dim `(batch_size, M) as
+           input, where M is the dimension of the latent space, and outputs a
+           tensor of dimension (batch_size, feature_dim1, feature_dim2).
+        """
+        super(BernoulliDecoder, self).__init__()
+        self.decoder_net = decoder_net
+        self.std = nn.Parameter(torch.ones(28, 28)*0.5, requires_grad=True)
+
+    def forward(self, z):
+        """
+        Given a batch of latent variables, return a Bernoulli distribution over the data space.
+
+        Parameters:
+        z: [torch.Tensor] 
+           A tensor of dimension `(batch_size, M)`, where M is the dimension of the latent space.
+        """
+        logits = self.decoder_net(z)
+        return td.Independent(td.Bernoulli(logits=logits), 2)
+
+
+class VAE(nn.Module):
+    """
+    Define a Variational Autoencoder (VAE) model.
+    """
+    def __init__(self, prior, decoder, encoder):
+        """
+        Parameters:
+        prior: [torch.nn.Module] 
+           The prior distribution over the latent space.
+        decoder: [torch.nn.Module]
+              The decoder distribution over the data space.
+        encoder: [torch.nn.Module]
+                The encoder distribution over the latent space.
+        """
+            
+        super(VAE, self).__init__()
+        self.prior = prior
+        self.decoder = decoder
+        self.encoder = encoder
+
+    def elbo(self, x):
+        """
+        Compute the ELBO for the given batch of data.
+
+        Parameters:
+        x: [torch.Tensor] 
+           A tensor of dimension `(batch_size, feature_dim1, feature_dim2, ...)`
+           n_samples: [int]
+           Number of samples to use for the Monte Carlo estimate of the ELBO.
+        """
+        q = self.encoder(x)
+        z = q.rsample()
+        reconstruction = self.decoder(z).log_prob(x)
+        log_qz = q.log_prob(z)
+        prior_dist = self.prior()
+        log_pz = prior_dist.log_prob(z)
+        elbo = (reconstruction + log_pz - log_qz).mean()
+        return elbo
+
+    def sample(self, n_samples=1):
+        """
+        Sample from the model.
+        
+        Parameters:
+        n_samples: [int]
+           Number of samples to generate.
+        """
+        z = self.prior().sample(torch.Size([n_samples]))
+        return self.decoder(z).sample()
+    
+    def forward(self, x):
+        """
+        Compute the negative ELBO for the given batch of data.
+
+        Parameters:
+        x: [torch.Tensor] 
+           A tensor of dimension `(batch_size, feature_dim1, feature_dim2)`
+        """
+        return -self.elbo(x)
+
+
+def train(model, optimizer, data_loader, epochs, device):
+    """
+    Train a VAE model.
+
+    Parameters:
+    model: [VAE]
+       The VAE model to train.
+    optimizer: [torch.optim.Optimizer]
+         The optimizer to use for training.
+    data_loader: [torch.utils.data.DataLoader]
+            The data loader to use for training.
+    epochs: [int]
+        Number of epochs to train for.
+    device: [torch.device]
+        The device to use for training.
+
+    Returns:
+    losses: [list of float]
+        Mean loss (negative ELBO) per epoch.
+    """
+    model.train()
+    losses = []
+
+    total_steps = len(data_loader)*epochs
+    progress_bar = tqdm(range(total_steps), desc="Training")
+
+    for epoch in range(epochs):
+        epoch_losses = []
+        data_iter = iter(data_loader)
+        for x in data_iter:
+            x = x[0].to(device)
+            optimizer.zero_grad()
+            loss = model(x)
+            loss.backward()
+            optimizer.step()
+            epoch_losses.append(loss.item())
+
+            # Update progress bar
+            progress_bar.set_postfix(loss=f"⠀{loss.item():12.4f}", epoch=f"{epoch+1}/{epochs}")
+            progress_bar.update()
+        losses.append(sum(epoch_losses) / len(epoch_losses))
+    return losses
+
+
+if __name__ == "__main__":
+    from torchvision import datasets, transforms
+    from torchvision.utils import save_image, make_grid
+    import matplotlib.pyplot as plt
+    import glob
+
+    # Parse arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', type=str, default='train', choices=['train', 'sample'], help='what to do when running the script (default: %(default)s)')
+    parser.add_argument('--model', type=str, default='model.pt', help='file to save model to or load model from (default: %(default)s)')
+    parser.add_argument('--samples', type=str, default='samples.png', help='file to save samples in (default: %(default)s)')
+    parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda', 'mps'], help='torch device (default: %(default)s)')
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N', help='batch size for training (default: %(default)s)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: %(default)s)')
+    parser.add_argument('--latent-dim', type=int, default=32, metavar='N', help='dimension of latent variable (default: %(default)s)')
+    parser.add_argument('--prior', type=str, default='gaussian', choices=['gaussian', 'mog'], help='prior distribution (default: %(default)s)')
+    parser.add_argument('--plot-loss', type=str, default='loss_curve.png', help='file to save loss curve plot (default: %(default)s)')
+
+    args = parser.parse_args()
+    # Default model path per prior to avoid overwriting
+    if args.model == 'model.pt':
+        args.model = f'model_{args.prior}.pt'
+    if args.plot_loss == 'loss_curve.png':
+        args.plot_loss = f'loss_curve_{args.prior}.png'
+    if args.samples == 'samples.png':
+        args.samples = f'samples_{args.prior}.png'
+    print('# Options')
+    for key, value in sorted(vars(args).items()):
+        print(key, '=', value)
+
+    device = args.device
+
+    # Load MNIST as binarized at 'thresshold' and create data loaders
+    thresshold = 0.5
+    mnist_train_loader = torch.utils.data.DataLoader(datasets.MNIST('data/', train=True, download=True,
+                                                                    transform=transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: (thresshold < x).float().squeeze())])),
+                                                    batch_size=args.batch_size, shuffle=True)
+    mnist_test_loader = torch.utils.data.DataLoader(datasets.MNIST('data/', train=False, download=True,
+                                                                transform=transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: (thresshold < x).float().squeeze())])),
+                                                    batch_size=args.batch_size, shuffle=True)
+
+    # Define prior distribution
+    M = args.latent_dim
+    if args.prior == 'gaussian':
+        prior = GaussianPrior(M)
+    elif args.prior == 'mog':
+        prior = MoGPrior(M, K=10, component_std=0.1)
+
+    # Define encoder and decoder networks
+    encoder_net = nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(784, 512),
+        nn.ReLU(),
+        nn.Linear(512, 512),
+        nn.ReLU(),
+        nn.Linear(512, M*2),
+    )
+
+    decoder_net = nn.Sequential(
+        nn.Linear(M, 512),
+        nn.ReLU(),
+        nn.Linear(512, 512),
+        nn.ReLU(),
+        nn.Linear(512, 784),
+        nn.Unflatten(-1, (28, 28))
+    )
+
+    # Define VAE model
+    decoder = BernoulliDecoder(decoder_net)
+    encoder = GaussianEncoder(encoder_net)
+    model = VAE(prior, decoder, encoder).to(device)
+
+    # Choose mode to run
+    if args.mode == 'train':
+        # Define optimizer
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+        # Train model
+        losses = train(model, optimizer, mnist_train_loader, args.epochs, args.device)
+
+        # Save model
+        torch.save(model.state_dict(), args.model)
+
+        # Plot loss curve
+        plt.figure(figsize=(8, 5))
+        plt.plot(losses, 'b-', linewidth=2)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss (negative ELBO)')
+        plt.title(f'Training Loss - {args.prior} prior')
+        plt.grid(True, alpha=0.3)
+        plt.tight_layout()
+        plt.savefig(args.plot_loss)
+        plt.close()
+        print(f"Loss curve saved to {args.plot_loss}")
+
+        # Generate and save samples
+        model.eval()
+        with torch.no_grad():
+            samples = (model.sample(64)).cpu()
+        save_image(samples.view(64, 1, 28, 28), args.samples)
+        print(f"Samples saved to {args.samples}")
+
+    elif args.mode == 'sample':
+        model.load_state_dict(torch.load(args.model, map_location=torch.device(args.device)))
+
+        # Generate samples
+        model.eval()
+        with torch.no_grad():
+            samples = (model.sample(64)).cpu() 
+            save_image(samples.view(64, 1, 28, 28), args.samples)
+
